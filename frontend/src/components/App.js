603c5058addd317a1025d2d56fb3ebc1da1cd6de
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-09-06 20:45:46 +0200

    done

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>React Django</h1>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <h1>React Django</h1>
+    </div>
+  );
 }
 
 const appDiv = document.getElementById("app");
