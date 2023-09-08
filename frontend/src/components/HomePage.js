a136649d5ee4ad2d1f62c453e9fc05007f3de4ad
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-09-08 13:26:35 +0200

    homepage styling

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,19 +1,40 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Typography from '@mui/material/Typography';
+import ExplanationCard from "./ExplanationCard";
+import Button from '@mui/material/Button';
 
 export default function HomePage() {
-
+  const cardContents = [
+    {
+      title: "Track your favourite shows",
+      content: "Add shows from all streaming services to your watchlist and keep track of your progress"
+    },
+    {
+      title: "Get notified of new episodes",
+      content: "Loved this show but the next season is not announced yet? Get an email it comes out"
+    },
+    {
+      title: "See show recommendations",
+      content: "Finished all your shows? Get recommendations for new shows based on your watchlist"
+    }
+  ]
   return (
     <div>
       <Navbar />
-      <div className="w-100 pt-16 text-center">
-        <h1 className="text-8xl font-light mb-8">Welcome to BingeWatcher</h1>
-        <h3 className="text-5xl font-light mb-8">Track your favorite shows and movies</h3>
+      <div className="w-100 pt-16 text-center mb-8">
+        <Typography variant="h1" component="div" sx={{ flexGrow: 1 }}>Welcome to BingeWatcher</Typography>
       </div>
-      <div className="homepage-explanation">
-        <div className="explanation-card">
-
-        </div>
+      <div className="text-center mb-16">
+        <Typography variant="h3" component="div" sx={{ flexGrow: 1 }}>Track all your favourite shows</Typography>
+      </div>
+      <div className="text-center w-100 mb-16">
+        <a href="/profile"><button className="bg-blue-500 hover:bg-blue-700 text-white font-light text-5xl py-4 px-10 rounded">Get started</button></a>
+      </div>
+      <div className="w-11/12 mx-auto flex justify-center">
+        {cardContents.map((cardContent) => (
+          <ExplanationCard title={cardContent.title} content={cardContent.content} />
+        ))}
       </div>
     </div>
   );
