6420c1dab52fe13d44dc337ec09eaa5c62156607
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-09-08 10:27:08 +0200

    switched css to tailwind

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -6,9 +6,9 @@ export default function HomePage() {
   return (
     <div>
       <Navbar />
-      <div className="homepage-banner text-center">
-        <h1>Welcome to BingeWatcher</h1>
-        <h3>Track your favorite shows and movies</h3>
+      <div className="w-100 pt-16 text-center">
+        <h1 className="text-8xl font-light mb-8">Welcome to BingeWatcher</h1>
+        <h3 className="text-5xl font-light mb-8">Track your favorite shows and movies</h3>
       </div>
       <div className="homepage-explanation">
         <div className="explanation-card">
