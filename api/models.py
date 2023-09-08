1994c788e669c9c32baf7dcbb6f9de67f1e52457
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-09-08 16:44:44 +0200

    first models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import requests
 
 # Create your models here.
+class Show(models.Model):
+    title = models.CharField(max_length=255)
+    network = models.CharField(max_length=255)
+    description = models.TextField()
+    tmdb_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def create_seasons(self):
+        url = "https://api.themoviedb.org/3/tv/{self.tmdb_id}?api_key={env('TMDB_API_KEY')}"
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        data = response.json()
+        number_of_seasons = data["number_of_seasons"]
+        for i in range(1, number_of_seasons + 1):
+            Season.objects.create(show=self, season_number=i)
+        return self
+
+
+class Season(models.Model):
+    show = models.ForeignKey(Show, related_name="seasons", on_delete=models.CASCADE)
+    season_number = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
