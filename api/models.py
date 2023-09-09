0c805f7dbe534eed79916364bd809115237ee82f
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-09-09 09:45:00 +0200

    done

--- api/models.py
+++ api/models.py
@@ -10,11 +10,13 @@ class Show(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def create_seasons(self):
-        url = "https://api.themoviedb.org/3/tv/{self.tmdb_id}?api_key={env('TMDB_API_KEY')}"
+        url = f'https://api.themoviedb.org/3/tv/{self.tmdb_id}?api_key=88d4e0c3270ccc984866aea7fb25036f'
+        print(url)
         payload = {}
         headers = {}
         response = requests.request("GET", url, headers=headers, data=payload)
         data = response.json()
+        print(data)
         number_of_seasons = data["number_of_seasons"]
         for i in range(1, number_of_seasons + 1):
             Season.objects.create(show=self, season_number=i)
